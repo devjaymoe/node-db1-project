1a4f66f320b71b3ece8b926a97fd5e4fdc756e12
Devin_Moore <d.j.moore209@gmail.com>
2020-05-11 16:14:04 -0400

    completed mvp, added crud ops and sql queries

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
-
+const AccountsRouter =  require('../accounts/accountsRouter.js');
 const server = express();
 
 server.use(express.json());
+
+server.use('/accounts', AccountsRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'server is up'})
+})
 
 module.exports = server;
